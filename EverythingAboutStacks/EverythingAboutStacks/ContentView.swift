6c82b922848dd478891794171faa1c1206dfae54
afsana farheen <69518729+afsana-cloud@users.noreply.github.com>
2023-04-02 22:31:22 +0530

    Add files via upload

---
+++ EverythingAboutStacks/EverythingAboutStacks/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  EverythingAboutStacks
+//
+//  Created by afsana-11711 on 25/03/23.
+//
+
+import SwiftUI
+
+
+struct UserView: View {
+    let users = [
+        User(name: "John", isActive: true, profileImage: "profile1"),
+        User(name: "Sarah", isActive: false, profileImage: "profile2"),
+        User(name: "David", isActive: true, profileImage:"profile6"),
+        User(name: "Emily", isActive: false, profileImage:"profile4"),
+        User(name: "Mike", isActive: true, profileImage:"profile5"),
+        User(name: "Mike", isActive: true, profileImage:"profile1")
+    ]
+
+    
+    var body: some View {
+        Form {
+            ForEach(users) { user in
+                HStack{
+                    ZStack(alignment: .leading) {
+                        Image(user.profileImage)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100,height: 100)
+                            .clipShape(Circle())
+                        Circle()
+                            .fill(user.statusColor)
+                            .frame(width: 15, height: 15).padding(.leading,80)
+                            .padding(.top,65)
+                    }
+                    VStack{
+                        Text(user.name).font(.title2)
+                        Text("Hello").font(.callout)
+                        Text("10:20pm").font(.caption)
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserView()
+    }
+}
+
+struct User:Identifiable {
+    var id = UUID()
+    let name: String
+    let isActive: Bool
+    let profileImage: String
+    
+    var statusColor: Color {
+        return isActive ? .green : .yellow
+    }
+}
+
---
+++ EverythingAboutStacks/EverythingAboutStacks/EverythingAboutStacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverythingAboutStacksApp.swift
+//  EverythingAboutStacks
+//
+//  Created by afsana-11711 on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct EverythingAboutStacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserView()
+        }
+    }
+}
